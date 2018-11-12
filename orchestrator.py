a0321923784a6d8a2bcb65e69b6bc77d6b705fcd
Sam Smith <mistmash@localhost.localdomain>
2018-11-12 16:39:33 +0000

    Simple python to query postman basic get/post server

---
+++ orchestrator.py
@@ -0,0 +1,19 @@
+import requests
+
+URL = 'https://3eb8d0f8-8826-41eb-a609-9e1ab92a0384.mock.pstmn.io/'
+statusURL = URL + 'status'
+startURL = URL + 'start'
+stopURL = URL + 'stop'
+
+print(statusURL)
+print(startURL)
+print(stopURL)
+
+response = requests.get(statusURL)
+print(response)
+
+response = requests.post(startURL)
+print(response)
+
+response = requests.post(stopURL)
+print(response)
